caace8cc5a6f32fb62466d8a04d18dfdcea87585
laurenwden <laurenm.weber@outlook.com>
2020-06-19 23:20:54 -0600

    models and database debugging complete -LW

--- goodsend/forms.py
+++ goodsend/forms.py
@@ -1,3 +1,17 @@
-from goodsend import FlaskForm
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, EqualTo, Email
+from wtforms.validators import DataRequired, EqualTo, Email, Length
+
+class UserInfoForm(FlaskForm):
+    first_name = StringField('first_name', validators=[DataRequired()])
+    last_name = StringField('last_name',validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    phone_number = StringField('Phone Number', validators=[DataRequired(), Length(min=10, max=10)])
+    password = PasswordField('Password',validators=[DataRequired()])
+    confirm_pass = PasswordField('Confirm Password',validators =[DataRequired(),EqualTo('password')])
+    submit = SubmitField()
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit= SubmitField()
--- goodsend/models.py
+++ goodsend/models.py
@@ -7,3 +7,58 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
 from flask_login import UserMixin
+
+class User(db.Model,UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.String(150), nullable=False)
+    last_name = db.Column(db.String(150), nullable = False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    phone_number = db.Column(db.String(150), nullable=True, unique=True)   
+    password = db.Column(db.String(256), nullable = False)
+
+
+    def __init__(self,first_name, last_name,email,phone_number, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.phone_number = phone_number
+        self.password = self.set_password(password)
+
+    def set_password(self,password):
+        self.pw_hash = generate_password_hash(password)
+        return self.pw_hash
+
+class Waitlist(db.Model,UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.String(150), nullable=False)
+    last_name = db.Column(db.String(150), nullable = False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    phone_number = db.Column(db.String(150), nullable=True, unique=True)   
+    password = db.Column(db.String(256), nullable = False)
+
+
+    def __init__(self,first_name, last_name,email,phone_number, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.phone_number = phone_number
+        self.password = self.set_password(password)
+
+ 
+
+class Approved(db.Model,UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.String(150), nullable=False)
+    last_name = db.Column(db.String(150), nullable = False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    phone_number = db.Column(db.String(150), nullable=True, unique=True)   
+    password = db.Column(db.String(256), nullable = False)
+
+
+    def __init__(self,first_name, last_name,email,phone_number, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.phone_number = phone_number
+        self.password = self.set_password(password)
+
